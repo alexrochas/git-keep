dd0b4284eedd5aa00e0afe7685715bf62b35407d
Alex Rocha <alex.rochas@yahoo.com.br>
2016-09-26 16:22:14 -0300

    fix: revert actual minus one

--- hold-git.sh
+++ hold-git.sh
@@ -10,7 +10,7 @@ actual_branch=`git rev-parse --abbrev-ref HEAD`
 `git add .`
 
 # commit and get SHA1
-`git commit -m 'HOLD gIT place holder'`
+`git commit -m "HOLD gIT place holder"`
 current_sha=`git rev-parse --short=5 --verify HEAD`
 
 # create new branch and move changes
@@ -21,7 +21,7 @@ current_sha=`git rev-parse --short=5 --verify HEAD`
 `git checkout $actual_branch`
 
 # revert last commit
-`git reset --hard HEAD`
+`git reset --hard HEAD~1`
 
 echo "Holding ${current_sha}_${branch_name}"
 
