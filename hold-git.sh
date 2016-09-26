50ad17769f39aabc39e71f5dd86e472cbff34695
Alex Rocha <alex.rochas@yahoo.com.br>
2016-09-26 16:01:55 -0300

    test: first version

---
+++ hold-git.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# get message for branch name
+branch_name=$1
+
+# save actual branch
+actual_branch=`git rev-parse --abbrev-ref HEAD`
+
+# add all unstaged things
+`git add .`
+
+# commit and get SHA1
+`git commit -m 'HOLD gIT place holder'`
+current_sha=`git rev-parse --short=5 --verify HEAD`
+
+# create new branch and move changes
+`git checkout -b ${current_sha}_${branch_name}`
+`git cherry-pick ${current_sha}_${branch_name}`
+
+# back to last branch
+`git checkout $actual_branch`
+
+# revert last commit
+`git reset --hard HEAD`
+
+echo "Holding ${current_sha}_${branch_name}"
+
+
