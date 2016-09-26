0a40e734e57c28164e213f13b3410aa07ea6ebe9
Alex Rocha <alex.rochas@yahoo.com.br>
2016-09-26 17:29:56 -0300

    test: first script to revert stashed branch

---
+++ revert_to.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# hold actual branch
+actual_branch=`git rev-parse --abbrev-ref HEAD`
+
+# checkout to stashed branch
+git checkout $1
+
+# get HEAD commit
+commit_to_apply_sha=`git rev-parse --short=5 --verify HEAD`
+
+# back to original branch
+git checkout $actual_branch
+
+# apply commit
+git cherry-pick $commit_to_apply
+
+# delete stashed branch
+git branch -D $1
+
+echo "Done!"
